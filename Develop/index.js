b743c327ebae94e68041f85cb113764845632501
Gabby <gabbympinto@gmail.com>
2023-03-25 14:22:47 -0400

    started questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,54 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    }
+].then(response => {
+    console.log(response);
+    writeToFile(response);
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const READMEString = '';
+    fs.writeFile("README.md", READMEString, (err) => err ? console.log(error) : console.log("Your README was created successfully!"));
+}
 
 // TODO: Create a function to initialize app
 function init() {}
