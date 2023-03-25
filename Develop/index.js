8b4c2d8a599e65ffcca5d7c85bbafbc194edccb2
Gabby <gabbympinto@gmail.com>
2023-03-25 19:25:16 -0400

    finished questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,38 +6,51 @@ const fs = require("fs");
 const questions = [
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What is your Github username?',
+        name: 'Github',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What is your email address?',
+        name: 'email',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What is your project name?',
+        name: 'title',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'Please write a short description of your project',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+        default: 'npm i',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+        default: 'npm test',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What does the user need to know about using this repo?',
+        name: 'information',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to this repo?',
+        name: 'contribution',
     }
 ].then(response => {
     console.log(response);
@@ -47,11 +60,13 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     const READMEString = '';
-    fs.writeFile("README.md", READMEString, (err) => err ? console.log(error) : console.log("Your README was created successfully!"));
+    fs.writeFile("README.md", READMEString, (err) => err ? console.log(error) : console.log("Your README was successfully created!"));
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
