218874b1716c5225161409544648c3ab2ab34c6b
Gabby <gabbympinto@gmail.com>
2023-03-25 20:20:37 -0400

    added README wireframe and started license functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = inquirer.prompt([
     {
         type: 'input',
         message: 'What is your Github username?',
@@ -45,28 +46,18 @@ const questions = [
     {
         type: 'input',
         message: 'What does the user need to know about using this repo?',
-        name: 'information',
+        name: 'usage',
     },
     {
         type: 'input',
         message: 'What does the user need to know about contributing to this repo?',
-        name: 'contribution',
+        name: 'contributing',
     }
-].then(response => {
-    console.log(response);
-    writeToFile(response);
+]) .then(data => {
+    writeToFile(data);
 });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const READMEString = '';
-    fs.writeFile("README.md", READMEString, (err) => err ? console.log(error) : console.log("Your README was successfully created!"));
+    fs.writeFile("README.md", generateMarkdown, (err) => err ? console.log(err) : console.log("Your README was successfully created!"));
 }
-
-// TODO: Create a function to initialize app
-function init() {
-
-}
-
-// Function call to initialize app
-init();
